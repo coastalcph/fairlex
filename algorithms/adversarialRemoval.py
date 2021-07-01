Bug in algorithms update (missing autocast + unscale before gradient clipping) + Update adversarialRemoval to sample batches uniformly over groups

algorithms/adversarialRemoval.py
@@ -69,15 +69,9 @@ class AdversarialRemoval(SingleModelAlgorithm):
         x = x.to(self.device)
         y_true = y_true.to(self.device)
         g = self.grouper.metadata_to_group(metadata).to(self.device)
-        if self.scaler:
-            with torch.cuda.amp.autocast():
-                features = self.featurizer(x)
-                outputs = self.classifier(features)
-                adv_outputs = self.adv_classifier(features)
-        else:
-            features = self.featurizer(x)
-            outputs = self.classifier(features)
-            adv_outputs = self.adv_classifier(features)
+        features = self.featurizer(x)
+        outputs = self.classifier(features)
+        adv_outputs = self.adv_classifier(features)
 
         # package the results
         results = {

algorithms/deepCORAL.py
@@ -76,13 +76,9 @@ class DeepCORAL(SingleModelAlgorithm):
         x = x.to(self.device)
         y_true = y_true.to(self.device)
         g = self.grouper.metadata_to_group(metadata).to(self.device)
-        if self.scaler:
-            with torch.cuda.amp.autocast():
-                features = self.featurizer(x)
-                outputs = self.classifier(features)
-        else:
-            features = self.featurizer(x)
-            outputs = self.classifier(features)
+        features = self.featurizer(x)
+        outputs = self.classifier(features)
+
         # package the results
         results = {
             'g': g,